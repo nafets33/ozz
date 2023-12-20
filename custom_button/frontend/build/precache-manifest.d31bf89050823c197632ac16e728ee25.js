1010d739133d83ba00dacc776c6e108e45461227
nafets33 <stapinski89gmail.com>
2023-12-20 13:45:04 -0500

    voiceGPT build dir

---
+++ custom_button/frontend/build/precache-manifest.d31bf89050823c197632ac16e728ee25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dcfd62cf0f1a29ecd52bbb2083e01dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81dd87730d1c00079d06",
+    "url": "./static/css/main.1aee6ce8.chunk.css"
+  },
+  {
+    "revision": "178d6879b4dad6ee44cc",
+    "url": "./static/js/2.e2287c3f.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.e2287c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81dd87730d1c00079d06",
+    "url": "./static/js/main.ad47c3a1.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
---
+++ custom_voiceGPT/frontend/build/precache-manifest.de95faeba916be7370e14664f953f542.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38666b9b58f80f57a06f6a94defa16e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d982a43e374cd0671ba0",
+    "url": "./static/js/2.a20ebcff.chunk.js"
+  },
+  {
+    "revision": "facab982d288decb294efc84eaaf5fc8",
+    "url": "./static/js/2.a20ebcff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dbc673f02839e6f17b2",
+    "url": "./static/js/main.1c080d60.chunk.js"
+  },
+  {
+    "revision": "70f07a1363ef928fd87d",
+    "url": "./static/js/runtime-main.aded3e07.js"
+  }
+]);
