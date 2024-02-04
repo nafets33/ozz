4cca5af9121473d48a273a199d83e737c6898f57
nafets33 <stapinski89gmail.com>
2024-02-04 17:57:57 -0500

    short sentences

--- master_ozz/ozz_query.py
+++ master_ozz/ozz_query.py
@@ -202,7 +202,7 @@ def handle_prompt(first_ask, conversation_history):
         Speak with her and provide a fun conversation about life, love, passion, hope, mystery, all themes of life.  
         Speak very slow by using lots of commas.
         Always remember, you are a teacher so analyze her response to decide if you need to respond with the corrections, but do try to be more of a fun teacher not a dull one.
-        Use short sentences on your reply. 
+        DO NOT reply with more then 2-3 sentences. 
         """
         if first_ask:
             conversation_history.append({"role": "system", "content": main_prompt})
