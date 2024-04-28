f771ea22d4adb262e47357bb43570e6cd4ef30d0
nafets33 <stapinski89gmail.com>
2024-04-28 17:47:18 -0400

    ui

--- pages/Characters.py
+++ pages/Characters.py
@@ -117,17 +117,17 @@ def ozz():
         with cols[0]:
             st.markdown(f''':yellow[{text}]''')
 
-        with cols[1]:
-            with st.expander("3 Ways to chat", True):
-                cols = st.columns((3,2))
-                with cols[0]:
-                    st.info("1: RECOMMENDED --> Click And Ask Button: Each time you click you can speak your question")
-                    st.info("2: Conversational Mode Button: Once you click, use Keyword 'Stefan', ex: 'Stefan How Are you today' (If stefan responds with a question you can directly answer it and don't need to say his name)")
-                    st.info("3: Chat Form: Type your questions and hit enter")
-                with cols[1]:
-                    st.error("Please note: Sometimes questions may be misunderstood and the response may result in inchorent manner.")
-                    st.warning("The LLM that uses RAG (i.e. this one) needs extra context to undestand each new query from user, Having responses tailor more accurately requires more engineering")
-                st.error("Character Speach ONLY works on Desktop and does not work on Mobile")
+        # with cols[1]:
+        with st.expander("3 Ways to chat", False):
+            cols = st.columns((3,2))
+            with cols[0]:
+                st.info("1: RECOMMENDED --> Click And Ask Button: Each time you click you can speak your question")
+                st.info("2: Conversational Mode Button: Once you click, use Keyword 'Stefan', ex: 'Stefan How Are you today' (If stefan responds with a question you can directly answer it and don't need to say his name)")
+                st.info("3: Chat Form: Type your questions and hit enter")
+            with cols[1]:
+                st.error("Please note: Sometimes questions may be misunderstood and the response may result in inchorent manner.")
+                st.warning("The LLM that uses RAG (i.e. this one) needs extra context to undestand each new query from user, Having responses tailor more accurately requires more engineering")
+            st.error("Character Speach ONLY works on Desktop and does not work on Mobile")
 
     else:
         embedding_default = []
